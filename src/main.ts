f09e95f47fc882bdf94829c06ca142fab943d460
mjas0006 <mjas0006@student.monash.edu>
2023-08-19 14:38:46 +1000

    Move rendering functions to view

--- src/main.ts
+++ src/main.ts
@@ -22,154 +22,38 @@ import { updateView } from "./view";
 import { fromEvent, interval, merge, Subscription, Observable } from "rxjs";
 import { map, filter, scan } from "rxjs/operators";
 
-
-
 const initialState: State = {
   gameEnd: false,
 } as const;
 
-/** Rendering (side effects) */
-
 /**
- * Displays a SVG element on the canvas. Brings to foreground.
- * @param elem SVG element to display
- */
-const show = (elem: SVGGraphicsElement) => {
-  elem.setAttribute("visibility", "visible");
-  elem.parentNode!.appendChild(elem);
-};
-
-/**
- * Hides a SVG element on the canvas.
- * @param elem SVG element to hide
- */
-const hide = (elem: SVGGraphicsElement) =>
-  elem.setAttribute("visibility", "hidden");
-
-/**
- * Creates an SVG element with the given properties.
- *
- * See https://developer.mozilla.org/en-US/docs/Web/SVG/Element for valid
- * element names and properties.
- *
- * @param namespace Namespace of the SVG element
- * @param name SVGElement name
- * @param props Properties to set on the SVG element
- * @returns SVG element
- */
-const createSvgElement = (
-  namespace: string | null,
-  name: string,
-  props: Record<string, string> = {}
-) => {
-  const elem = document.createElementNS(namespace, name) as SVGElement;
-  Object.entries(props).forEach(([k, v]) => elem.setAttribute(k, v));
-  return elem;
-};
-
-/**
- * This is the function called on page load. Your main game loop
- * should be called here.
+ * Main game loop called on window load. See bottom of file.
  */
 export function main() {
-  // Canvas elements
-  const svg = document.querySelector("#svgCanvas") as SVGGraphicsElement &
-    HTMLElement;
-  const preview = document.querySelector("#svgPreview") as SVGGraphicsElement &
-    HTMLElement;
-  const gameover = document.querySelector("#gameOver") as SVGGraphicsElement &
-    HTMLElement;
-  const container = document.querySelector("#main") as HTMLElement;
 
-  svg.setAttribute("height", `${Viewport.CANVAS_HEIGHT}`);
-  svg.setAttribute("width", `${Viewport.CANVAS_WIDTH}`);
-  preview.setAttribute("height", `${Viewport.PREVIEW_HEIGHT}`);
-  preview.setAttribute("width", `${Viewport.PREVIEW_WIDTH}`);
-
-  // Text fields
-  const levelText = document.querySelector("#levelText") as HTMLElement;
-  const scoreText = document.querySelector("#scoreText") as HTMLElement;
-  const highScoreText = document.querySelector("#highScoreText") as HTMLElement;
-
-  /** User input */
-
+  // General Keypress stream
   const key$ = fromEvent<KeyboardEvent>(document, "keypress");
 
+  // Function to filter keypress stream by key
   const fromKey = (keyCode: Key) =>
     key$.pipe(filter(({ code }) => code === keyCode));
 
+  // Keypress stream per key
   const left$ = fromKey("KeyA").pipe(map(_ => new ShiftLeft()));
   const right$ = fromKey("KeyD").pipe(map(_ => new ShiftRight()));
   const down$ = fromKey("KeyS").pipe(map(_ => new Down()));
-
-  /** Observables */
-
-  /** Determines the rate of time steps */
   const tick$ = interval(GameConstants.TICK_RATE_MS).pipe(map(elapsed => new Tick(elapsed)));
 
+  // Merge observables to action stream
   const action$: Observable<Action> = merge(tick$, left$, right$, down$);
+  // Accumulate actions in state
   const state$: Observable<State> = action$.pipe(scan(reduceState, initialState));
-  const subscription: Subscription = state$.subscribe(updateView(()=>subscription.unsubscribe()));
+  // Render state using subscription
+  const subscription: Subscription = state$.subscribe(updateView(() => subscription.unsubscribe()));
 
-  /**
-   * Renders the current state to the canvas.
-   *
-   * In MVC terms, this updates the View using the Model.
-   *
-   * @param s Current state
-   */
-  const render = (s: State) => {
-    // Add blocks to the main grid canvas
-    const cube = createSvgElement(svg.namespaceURI, "rect", {
-      height: `${Block.HEIGHT}`,
-      width: `${Block.WIDTH}`,
-      x: "0",
-      y: "0",
-      style: "fill: green",
-    });
-    svg.appendChild(cube);
-    const cube2 = createSvgElement(svg.namespaceURI, "rect", {
-      height: `${Block.HEIGHT}`,
-      width: `${Block.WIDTH}`,
-      x: `${Block.WIDTH * (3 - 1)}`,
-      y: `${Block.HEIGHT * (20 - 1)}`,
-      style: "fill: red",
-    });
-    svg.appendChild(cube2);
-    const cube3 = createSvgElement(svg.namespaceURI, "rect", {
-      height: `${Block.HEIGHT}`,
-      width: `${Block.WIDTH}`,
-      x: `${Block.WIDTH * (4 - 1)}`,
-      y: `${Block.HEIGHT * (20 - 1)}`,
-      style: "fill: red",
-    });
-    svg.appendChild(cube3);
-
-    // Add a block to the preview canvas
-    const cubePreview = createSvgElement(preview.namespaceURI, "rect", {
-      height: `${Block.HEIGHT}`,
-      width: `${Block.WIDTH}`,
-      x: `${Block.WIDTH * 2}`,
-      y: `${Block.HEIGHT}`,
-      style: "fill: green",
-    });
-    preview.appendChild(cubePreview);
-  };
-
-  const source$ = merge(tick$)
-    .pipe(scan((s: State) => ({ gameEnd: true }), initialState))
-    .subscribe((s: State) => {
-      render(s);
-
-      if (s.gameEnd) {
-        show(gameover);
-      } else {
-        hide(gameover);
-      }
-    });
 }
 
-// The following simply runs your main function on window load.  Make sure to leave it in place.
+// Execute main function on load
 if (typeof window !== "undefined") {
   window.onload = () => {
     main();
--- src/view.ts
+++ src/view.ts
@@ -1,23 +1,78 @@
-// Function to update the view
+// Functions to render the view based on the model
 
 export { updateView }
 
 import { State } from "./types"
+import { Viewport, Block } from "./constants";
+
+/** Reusable rendering (side effects) function*/
+
 /**
- * Update the SVG game view.  
- * 
+ * Displays a SVG element on the canvas. Brings to foreground.
+ * @param elem SVG element to display
+ */
+const show = (elem: SVGGraphicsElement) => {
+    elem.setAttribute("visibility", "visible");
+    elem.parentNode!.appendChild(elem);
+};
+
+/**
+ * Hides a SVG element on the canvas.
+ * @param elem SVG element to hide
+ */
+const hide = (elem: SVGGraphicsElement) =>
+    elem.setAttribute("visibility", "hidden");
+
+/**
+ * Creates an SVG element with the given properties.
+ *
+ * See https://developer.mozilla.org/en-US/docs/Web/SVG/Element for valid
+ * element names and properties.
+ *
+ * @param namespace Namespace of the SVG element
+ * @param name SVGElement name
+ * @param props Properties to set on the SVG element
+ * @returns SVG element
+ */
+const createSvgElement = (
+    namespace: string | null,
+    name: string,
+    props: Record<string, string> = {}
+) => {
+    const elem = document.createElementNS(namespace, name) as SVGElement;
+    Object.entries(props).forEach(([k, v]) => elem.setAttribute(k, v));
+    return elem;
+};
+
+/**
+ * Renders the current state to the canvas.
+ *
+ * In MVC terms, this updates the View using the Model.
+ *
  * @param onFinish a callback function to be applied when the game ends.  For example, to clean up subscriptions.
  * @param s the current game model State
  * @returns void
  */
 function updateView(onFinish: () => void) {
-    return function (s: State):void {
-        const
-            svg = document.getElementById("svgCanvas"),
-            ship = document.getElementById("ship")
+    return function (s: State): void {
+        // Canvas elements
+        const svg = document.querySelector("#svgCanvas") as SVGGraphicsElement &
+            HTMLElement;
+        const preview = document.querySelector("#svgPreview") as SVGGraphicsElement &
+            HTMLElement;
+        const gameover = document.querySelector("#gameOver") as SVGGraphicsElement &
+            HTMLElement;
+        const container = document.querySelector("#main") as HTMLElement;
 
-        // if getElement is null, exit function early without doing anything
-        if (!svg || !ship) return
+        svg.setAttribute("height", `${Viewport.CANVAS_HEIGHT}`);
+        svg.setAttribute("width", `${Viewport.CANVAS_WIDTH}`);
+        preview.setAttribute("height", `${Viewport.PREVIEW_HEIGHT}`);
+        preview.setAttribute("width", `${Viewport.PREVIEW_WIDTH}`);
+
+        // Text fields
+        const levelText = document.querySelector("#levelText") as HTMLElement;
+        const scoreText = document.querySelector("#scoreText") as HTMLElement;
+        const highScoreText = document.querySelector("#highScoreText") as HTMLElement;
 
         // document.getElementById can return null
         // so use optional chaining to safely access method on element
@@ -25,8 +80,54 @@ function updateView(onFinish: () => void) {
             condition ? e?.classList.remove('hidden')
                 : e?.classList.add('hidden'))(document.getElementById(id))
 
+        /**
+         * Renders the current state to the canvas.
+         *
+         * In MVC terms, this updates the View using the Model.
+         *
+         * @param s Current state
+         */
+        // Add blocks to the main grid canvas
+        const cube = createSvgElement(svg.namespaceURI, "rect", {
+            height: `${Block.HEIGHT}`,
+            width: `${Block.WIDTH}`,
+            x: "0",
+            y: "0",
+            style: "fill: green",
+        });
+        svg.appendChild(cube);
+        const cube2 = createSvgElement(svg.namespaceURI, "rect", {
+            height: `${Block.HEIGHT}`,
+            width: `${Block.WIDTH}`,
+            x: `${Block.WIDTH * (3 - 1)}`,
+            y: `${Block.HEIGHT * (20 - 1)}`,
+            style: "fill: red",
+        });
+        svg.appendChild(cube2);
+        const cube3 = createSvgElement(svg.namespaceURI, "rect", {
+            height: `${Block.HEIGHT}`,
+            width: `${Block.WIDTH}`,
+            x: `${Block.WIDTH * (4 - 1)}`,
+            y: `${Block.HEIGHT * (20 - 1)}`,
+            style: "fill: red",
+        });
+        svg.appendChild(cube3);
+
+        // Add a block to the preview canvas
+        const cubePreview = createSvgElement(preview.namespaceURI, "rect", {
+            height: `${Block.HEIGHT}`,
+            width: `${Block.WIDTH}`,
+            x: `${Block.WIDTH * 2}`,
+            y: `${Block.HEIGHT}`,
+            style: "fill: green",
+        });
+        preview.appendChild(cubePreview);
+
+
         if (s.gameEnd) {
-            
+            show(gameover);
+        } else {
+            hide(gameover);
         }
     }
 }
