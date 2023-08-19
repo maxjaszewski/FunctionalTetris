751fcbc61843c594dd56f834b4a81f4b0f8f2564
mjas0006 <mjas0006@student.monash.edu>
2023-08-19 13:09:09 +1000

    Add constants import to main

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@
 
 import "./style.css";
 
+import { Constants, Viewport, Block } from "./constants";
 import { fromEvent, interval, merge } from "rxjs";
 import { map, filter, scan } from "rxjs/operators";
 
